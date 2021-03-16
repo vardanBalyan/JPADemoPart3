72015f28800a4420d4071b90b5d7c64682d7ab35
vardanttn <vardan.balyan@tothenew.com>
2021-03-16 12:20:52 +0530

    initial commit

---
+++ src/main/java/com/ttn/JPADemoPart3/JpaDemoPart3Application.java
@@ -0,0 +1,13 @@
+package com.ttn.JPADemoPart3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaDemoPart3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaDemoPart3Application.class, args);
+	}
+
+}
